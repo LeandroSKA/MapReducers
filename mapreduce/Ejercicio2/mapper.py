c378d93b8ff215477bd2e6d4c5fa042e1bac0347
F9Red70fokGPvAEH/7M1BQ== <xuwira37@cdh61-login6.bd.cluster.cesga.es>
2022-12-05 18:00:33 +0100

    Segundo ejercicio

--- mapreduce/Ejercicio2/mapper.py
+++ mapreduce/Ejercicio2/mapper.py
@@ -10,5 +10,6 @@ import sys
 
 for line in sys.stdin:
     data = line.strip().split("\t")
-    date, time, store, item, cost, payment = data
-    print(item+"\t"+cost)
+    if len(data)==6:
+        date, time, store, item, cost, payment = data
+        print(item+"\t"+cost)
